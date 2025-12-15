db9b56f51bcf2cc5314bbaddf2638ac4b7ce64ab
Ahmet KAYA <ahmet.kaya.ceng@gmail.com>
2025-12-15 10:01:39 +0300

    fix: updated models index for render database ssl connection

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,72 +1,60 @@
-﻿const { sequelize } = require('../config/database');
+﻿'use strict';
 
-// Import models
-const User = require('./User')(sequelize);
-const Department = require('./Department')(sequelize);
-const Student = require('./Student')(sequelize);
-const Faculty = require('./Faculty')(sequelize);
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const db = {};
 
-// Define associations
-User.hasOne(Student, {
-    foreignKey: 'user_id',
-    as: 'student'
-});
-Student.belongsTo(User, {
-    foreignKey: 'user_id',
-    as: 'user'
-});
+let sequelize;
 
-User.hasOne(Faculty, {
-    foreignKey: 'user_id',
-    as: 'faculty'
-});
-Faculty.belongsTo(User, {
-    foreignKey: 'user_id',
-    as: 'user'
-});
-
-Department.hasMany(Student, {
-    foreignKey: 'department_id',
-    as: 'students'
-});
-Student.belongsTo(Department, {
-    foreignKey: 'department_id',
-    as: 'department'
-});
-
-Department.hasMany(Faculty, {
-    foreignKey: 'department_id',
-    as: 'faculty'
-});
-Faculty.belongsTo(Department, {
-    foreignKey: 'department_id',
-    as: 'department'
-});
-
-Faculty.hasMany(Student, {
-    foreignKey: 'advisor_id',
-    as: 'advisees'
-});
-Student.belongsTo(Faculty, {
-    foreignKey: 'advisor_id',
-    as: 'advisor'
-});
-
-// Sync database
-const syncDatabase = async () => {
-    try {
-        await sequelize.sync({ alter: true });
-        console.log('✅ Database synchronized successfully.');
-    } catch (error) {
-        console.error('❌ Database sync error:', error);
+// KESİN ÇÖZÜM: Render'da DB_URL varsa onu kullan, yoksa config'e bak
+if (process.env.DB_URL) {
+    sequelize = new Sequelize(process.env.DB_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false // Render için kritik ayar
+            }
+        },
+        logging: false
+    });
+} else {
+    // Localde çalışırken burası çalışır
+    const config = require(__dirname + '/../config/config.json')[env];
+    if (config.use_env_variable) {
+        sequelize = new Sequelize(process.env[config.use_env_variable], config);
+    } else {
+        sequelize = new Sequelize(config.database, config.username, config.password, config);
     }
-};
+}
 
-module.exports = {
-    sequelize,
-    User,
-    Department,
-    Student,
-    Faculty,
-    syncDatabase
-};
+fs
+    .readdirSync(__dirname)
+    .filter(file => {
+        return (
+            file.indexOf('.') !== 0 &&
+            file !== basename &&
+            file.slice(-3) === '.js' &&
+            file.indexOf('.test.js') === -1
+        );
+    })
+    .forEach(file => {
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+        db[model.name] = model;
+    });
+
+Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
